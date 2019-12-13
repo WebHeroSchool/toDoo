a22cfd1195516ae99c838106d8165924a82ae19f
sergey gerasimov <sergey.gerasimov@realize-it.ru>
2019-12-13 12:11:04 +0300

    ad jsx

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const name = "sergey";
+const isOpen = true;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={{background: "grey"}}>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          webheroschool
-        </a>
+        <p>Привет {name}</p>
+        <p>{123}</p>
+        <p>{123+124}</p>
+        <p>{123+124}</p>
+       <p>{123>124}</p>
+       <p>{isOpen ? "открыто":"закрыто"}</p>
+       <p>{null}</p>
+       <p>{undefined}</p>
+       <p>{false}</p>
+       <p>{true}</p>
       </header>
     </div>
   );
