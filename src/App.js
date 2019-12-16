31b1a63d4cd65449aaf79cba19d2e0997b0b1692
sergey gerasimov <sergey.gerasimov@realize-it.ru>
2019-12-16 07:55:13 +0300

    add component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const element= (
-    <div>hello word</div>
-)
+const ItemList = ()=>{
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+        </ul>
+    )
+};
+const App = ()=>(<div>
+    <h1>ToDo</h1>
+    <ItemList/>
+</div>);
 
-export  {element};
+
+
+export default App;
