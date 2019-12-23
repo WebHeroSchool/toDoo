81648aa8f273e153750e8edde59c914baabe9028
sergey gerasimov <sergey.gerasimov@realize-it.ru>
2019-12-23 18:47:04 +0300

    add text field

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
 const InputItem =()=>{
 	return(
-		<input type="text"/>
+		<TextField  label="дело"
+		            id="outlined-size-normal"
+		            defaultValue="введите таск"
+		            variant="outlined"
+		            size="small"/>
 	)
 }
 export default InputItem;
